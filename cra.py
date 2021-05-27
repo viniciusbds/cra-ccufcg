b2d06e133888f1474b54163f7a3e468473cd6cb7
Vinicius Barbosa da Silva <vinicius.barbosa@virtus.ufcg.edu.br>
2021-05-27 11:52:56 -0300

    Ordenando as disciplinas de acordo com o plano de curso oficial

--- cra.py
+++ cra.py
@@ -4,42 +4,23 @@
 Script que armazena as notas das disciplinas e calcula o cra durante graduação de Ciência da Computação - UFCG
 '''
 
-S1 = {"p1": (-1, 60), "lp1": (-1, 60), "calculo1": (-1, 60), "vetorial": (-1, 60), "lpt": (-1, 60), "ic": (-1, 60)}
+S1 = {"FMCCI": (-1, 60), "p1": (-1, 60), "lp1": (-1, 60), "IC": (-1, 60), "opt geral": (-1, 60)}
 
-S2 = {"p2": (-1, 60), "lp2": (-1, 60), "calculo2": (-1, 60), "fmcc2": (-1, 60), "grafos": (-1, 60),"fmcc1": (-1, 60), "l.port": (-1, 60)}
+S2 = {"FMCCII": (-1, 60), "calculo1": (-1, 60), "p2": (-1, 60), "lp2": (-1, 60), "optativa geral": (-1, 60)}
 
-S3 = {"eda": (-1, 60), "leda": (-1, 60), "logica": (-1, 60), "a.linear": (-1, 60)}
+S3 = {"a.linear": (-1, 60), "logica": (-1, 60), "calculo2": (-1, 60), "EDA": (-1, 60), "LEDA": (-1, 60), "grafos": (-1, 60)}
 
-S4 = {"psoft": (-1, 60), "bd": (-1, 60), "oac": (-1, 60), "loac": (-1, 60), "prob": (-1, 60)}
+S4 = {"probabilidade": (-1, 60), "psoft": (-1, 60), "plp": (-1, 60), "Bd 1": (-1, 60), "OAC": (-1, 60), "LOAC": (-1, 60)}
 
-S5 = {"plp": (-1, 60),  "so": (-1, 60), "proj.so": (-1, 60), "eng.software": (-1, 60), "direito": (-1, 60)}
+S5 = {"estatistica": (-1, 60),  "AS": (-1, 60), "ES": (-1, 60), "redes": (-1, 60), "SO": (-1, 60), "TC": (-1, 60)}
 
-S6 = { "tc": (-1, 60), "as":(-1, 60), "redes": (-1, 60), "estatistica": (-1, 60), "concorrente": (-1, 60), "didatica-p2": (-1, 30)}
+S6 = { "MC": (-1, 60), "concorrente":(-1, 60), "IA": (-1, 60), "opt.especifica1": (-1, 60), "opt.especifica2": (-1, 60)}
 
-S7 = {"atal": (-1, 60), "met.cientifica": (-1, 60), "i.a": (-1, 60), "segurança": (-1,60)}
+S7 = {"ATAL": (-1, 60), "compiladores": (-1, 60), "opt.especifica1": (-1, 60), "opt.especifica2": (-1,60), "opt geral": (-1, 60)}
 
-S8 = {
-	"compiladores": (-1, 60),
-	"projeto1": (-1, 60),
-	"optativa1": (-1, 60),
-	"optativa2": (-1, 60),
-	"optativa.geral": (-1, 60),
-	"didatica": (-1, 30)
-}
+S8 = {"projeto1": (-1, 60), "PTCC": (-1, 60), "opt.especifica1": (-1, 60), "opt.especifica2": (-1,60), "opt geral": (-1, 60)}
 
-S9 = {
-	"projeto TCC": (-1, 60),
-	"projeto2.b3": (-1, 60),
-	"AA2": (-1, 60),
-	"optativa2": (-1, 60)
-}
-
-S10 = {
-	"TCC": (-1, 60),
-	"AA3": (-1, 60),
-	"optativa2": (-1, 60),
-	"optativa3": (-1, 60)
-}
+S9 = {"projeto2": (-1, 60), "TCC": (-1, 60), "opt.especifica1": (-1, 60), "opt.especifica2": (-1,60), "opt.especifica3": (-1,60), "opt.especifica4": (-1, 60)}
 
 
 def cra(semestres):
@@ -74,7 +55,7 @@ def progresso(semestres):
 
 	return cadeiras_pagas
 
-semestres = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10]
+semestres = [S1, S2, S3, S4, S5, S6, S7, S8, S9]
 
 print("## Informações Acadêmicas ##\n")
 print("CADEIRAS PAGAS: %d / 51" % progresso(semestres))
