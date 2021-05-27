c28a284b52ea791af0044586fd3296bd84612a59
Vinicius Barbosa da Silva <vinicius.barbosa@virtus.ufcg.edu.br>
2021-05-27 01:02:03 -0300

    Corrige bug: quando existe nota igual a 0

--- cra.py
+++ cra.py
@@ -4,41 +4,41 @@
 Script que armazena as notas das disciplinas e calcula o cra durante graduação de Ciência da Computação - UFCG
 '''
 
-S1 = {"p1": (0, 60), "lp1": (0, 60), "calculo1": (0, 60), "vetorial": (0, 60), "lpt": (0, 60), "ic": (0, 60)}
+S1 = {"p1": (-1, 60), "lp1": (-1, 60), "calculo1": (-1, 60), "vetorial": (-1, 60), "lpt": (-1, 60), "ic": (-1, 60)}
 
-S2 = {"p2": (0, 60), "lp2": (0, 60), "calculo2": (0, 60), "fmcc2": (0, 60), "grafos": (0, 60),"fmcc1": (0, 60), "l.port": (0, 60)}
+S2 = {"p2": (-1, 60), "lp2": (-1, 60), "calculo2": (-1, 60), "fmcc2": (-1, 60), "grafos": (-1, 60),"fmcc1": (-1, 60), "l.port": (-1, 60)}
 
-S3 = {"eda": (0, 60), "leda": (0, 60), "logica": (0, 60), "a.linear": (0, 60)}
+S3 = {"eda": (-1, 60), "leda": (-1, 60), "logica": (-1, 60), "a.linear": (-1, 60)}
 
-S4 = {"psoft": (0, 60), "bd": (0, 60), "oac": (0, 60), "loac": (0, 60), "prob": (0, 60)}
+S4 = {"psoft": (-1, 60), "bd": (-1, 60), "oac": (-1, 60), "loac": (-1, 60), "prob": (-1, 60)}
 
-S5 = {"plp": (0, 60),  "so": (0, 60), "proj.so": (0, 60), "eng.software": (0, 60), "direito": (0, 60)}
+S5 = {"plp": (-1, 60),  "so": (-1, 60), "proj.so": (-1, 60), "eng.software": (-1, 60), "direito": (-1, 60)}
 
-S6 = { "tc": (0, 60), "as":(0, 60), "redes": (0, 60), "estatistica": (0, 60), "concorrente": (0, 60), "didatica-p2": (0, 30)}
+S6 = { "tc": (-1, 60), "as":(-1, 60), "redes": (-1, 60), "estatistica": (-1, 60), "concorrente": (-1, 60), "didatica-p2": (0, 30)}
 
-S7 = {"atal": (0, 60), "met.cientifica": (0, 60), "i.a": (0, 60), "segurança": (0,60)}
+S7 = {"atal": (-1, 60), "met.cientifica": (-1, 60), "i.a": (-1, 60), "segurança": (-1,60)}
 
 S8 = {
-	"compiladores": (0, 60),
-	"projeto1": (0, 60),
-	"optativa1": (0, 60),
-	"optativa2": (0, 60),
-	"optativa.geral": (0, 60),
-	"didatica": (0, 30)
+	"compiladores": (-1, 60),
+	"projeto1": (-1, 60),
+	"optativa1": (-1, 60),
+	"optativa2": (-1, 60),
+	"optativa.geral": (-1, 60),
+	"didatica": (-1, 30)
 }
 
 S9 = {
-	"projeto TCC": (0, 60),
-	"projeto2.b3": (0, 60),
-	"AA2": (0, 60),
-	"optativa2": (0, 60)
+	"projeto TCC": (-1, 60),
+	"projeto2.b3": (-1, 60),
+	"AA2": (-1, 60),
+	"optativa2": (-1, 60)
 }
 
 S10 = {
-	"TCC": (0, 60),
-	"AA3": (0, 60),
-	"optativa2": (0, 60),
-	"optativa3": (0, 60)
+	"TCC": (-1, 60),
+	"AA3": (-1, 60),
+	"optativa2": (-1, 60),
+	"optativa3": (-1, 60)
 }
 
 
@@ -49,7 +49,7 @@ def cra(semestres):
 		for nome in semestre.keys():
 			disciplina = semestre[nome]
 			nota, carga  = disciplina[0],  disciplina[1]
-			if (nota != 0): # disciplinas com notas iguais a zero são ignoradas, ou seja, ainda não foram cursadas
+			if (nota >= 0): # disciplinas com notas iguais a zero são ignoradas, ou seja, ainda não foram cursadas
 				produto += nota * carga
 				horas += carga
 
